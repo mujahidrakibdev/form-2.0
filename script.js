af54fe72623b44e5cacc52417eee3891a1776710
Mujahid <mujahidrakibdev@gmail.com>
2025-03-10 00:03:33 +0600

    completed i think

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const prevBtn = document.querySelector(".prev")
-const nextBtn = document.querySelector(".next")
-const submitBtn = document.querySelector(".submit")
-const datas = document.querySelectorAll(".data")
-const dots = document.querySelectorAll(".nav-ul li")
-const progressBar = document.querySelector(".progress-bar")
+const form = document.querySelector(".form")
+
+const prevBtn = form.querySelector(".prev")
+const nextBtn = form.querySelector(".next")
+const submitBtn = form.querySelector(".submit")
+const dataCont = form.querySelector(".data-cont")
+const datas = form.querySelectorAll(".data")
+const dots = form.querySelectorAll(".nav-ul li")
+const progressBar = form.querySelector(".progress-bar")
 
 let round = 0
 
 nextBtn.addEventListener("click", (e)=> {
     e.preventDefault()
+
+    if (!isValid()) return
+
     if (round < 3) {
         round++   
     }
@@ -29,6 +35,26 @@ prevBtn.addEventListener("click", (e)=> {
         data.style.transform = `translateX(-${round * 100}%)`
     });
     
+})
+submitBtn.addEventListener("click", (e)=> {
+    e.preventDefault()
+    
+})
+
+const inputs = form.querySelectorAll("input, textarea")
+
+inputs.forEach(input => {
+    input.addEventListener("focus", (e) => {
+        const focInput = e.target
+        const focData = [...datas].findIndex((data) => {
+            return data.contains(focInput)
+        })
+
+        if (focData !== -1 && focData !== round) {
+            round = focData
+            updateBar()
+        }
+    })
 })
 
 function updateBar() {
@@ -54,3 +80,7 @@ function updateBar() {
 }
 
 
+function isValid() {
+    const fields = datas[round].querySelectorAll("input, textarea")
+    return [...fields].every(field => field.reportValidity())   
+}
